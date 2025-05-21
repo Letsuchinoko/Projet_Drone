1a8182bf914290416a145d85f557c62c018969a0
RayKill <rayan.djou@gmail.com>
2025-05-21 17:37:50 +0200

    Update API

--- Code/Code_API_Drone/app/src/main/java/com/example/APPLI/Fragment1.kt
+++ Code/Code_API_Drone/app/src/main/java/com/example/APPLI/Fragment1.kt
@@ -1,59 +1,111 @@
 package com.example.APPLI
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Fragment1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var textView: TextView
+    private lateinit var btnPermissionsBT: Button
+    private lateinit var btnActiverBT: Button
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothAdapter: BluetoothAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    // Effectue la demande des autorisations nécessaires
+    // Si tout est OK, cela activera le bouton « Activer BT »
+    private val requetePermissions: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions.values.all { it }) {
+                btnActiverBT.isEnabled = true
+                Toast.makeText(activity, "Permissions OK", Toast.LENGTH_SHORT).show()
+            }
         }
-    }
+
+    // Gère la demande d'activation du Bluetooth
+    private val requeteActivationBT =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Toast.makeText(activity, "L'utilisateur a activé le BT",
+                    Toast.LENGTH_SHORT).show()
+                // Activer un bouton (A COMPLETER PLUS TARD)
+            } else {
+                Toast.makeText(activity, "L'utilisateur a refusé",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_1, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // Initialisation des vues
+        textView = view.findViewById(R.id.textView)
+        btnPermissionsBT = view.findViewById(R.id.btnPermissionsBT)
+        btnActiverBT = view.findViewById(R.id.btnActiverBT)
+
+        // Configuration initiale
+        textView.text = "En attente des permissions Bluetooth..."
+        btnActiverBT.isEnabled = false
+
+        // Gestion des clics sur les boutons avec Toast
+        btnPermissionsBT.setOnClickListener {
+            lateinit var BT_PERMISSIONS: Array<String>
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                BT_PERMISSIONS = arrayOf(
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN
+                )
+            } else {
+                BT_PERMISSIONS = arrayOf(
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
             }
+            requetePermissions.launch(BT_PERMISSIONS)
+        }
+
+        btnActiverBT.setOnClickListener {
+            if (bluetoothAdapter.isEnabled) {
+                Toast.makeText(activity, "BT déjà activé", Toast.LENGTH_SHORT).show()
+                // Activer un bouton (A COMPLETER PLUS TARD)
+            } else {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                requeteActivationBT.launch(enableBtIntent)
+            }
+        }
+
+        // Tester si l'appareil ciblé autorise l'interface BT
+        bluetoothManager = requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            // L'appareil n'est pas doté d'une interface BT
+            Toast.makeText(activity, "La machine ne possède pas le Bluetooth",
+                Toast.LENGTH_SHORT).show()
+        } else {
+            btnPermissionsBT.isEnabled = true
+            Toast.makeText(activity, "Interface BT existe", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
--- Code/Code_API_Drone/app/src/main/java/com/example/APPLI/Fragment3.kt
+++ Code/Code_API_Drone/app/src/main/java/com/example/APPLI/Fragment3.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,6 +34,7 @@ class Fragment3 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Toast.makeText(context, "Niveau sonore", Toast.LENGTH_SHORT).show()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_3, container, false)
     }
